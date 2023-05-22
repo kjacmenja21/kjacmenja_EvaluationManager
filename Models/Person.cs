348cbe5a01ed05f8a2afa6edcb458c61f8af7977
kjacmenja21 <kjacmenja21@student.foi.hr>
2023-05-22 10:08:46 +0200

    Initial repository

---
+++ Models/Person.cs
@@ -0,0 +1,15 @@
+﻿
+namespace Evaluation_Manager.Models
+{
+    public abstract class Person : object
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public override string ToString()
+        {
+            return FirstName + " " + LastName;
+        }
+    }
+}
